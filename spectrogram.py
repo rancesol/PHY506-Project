e0694c669b1f112c73fb938a669e141542bd4597
rancesol <rancesol@buffalo.edu>
2019-04-24 13:35:25 -0400

    Creates a spectrogram of the infall. I still need to figure out the low frequency dominance towards the end -- it should not be there.

--- spectrogram.py
+++ spectrogram.py
@@ -5,12 +5,12 @@ from read_plot import *
 
 np.random.seed(19680801)
 
-t, w = read_plot("omega.data")
+t, w, amp = read_plot("omega.data")
 
 dt = 0.01
 s1 = []
 for i in range(len(t)) :
-    s1.append(np.sin(w[i]*t[i]))
+    s1.append(amp[i]*np.sin(w[i]*t[i]))
 
 nse = 0.01*np.random.random(size=len(t))
 
@@ -19,6 +19,6 @@ NFFT = 1024
 Fs = int(5.0 / dt)
 
 fig, (ax1, ax2) = plt.subplots(nrows=2)
-ax1.plot(t,x)
+ax1.plot(t,x, linewidth=.7)
 Pxx, freqs, bins, im = ax2.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=1000)
 plt.show()
