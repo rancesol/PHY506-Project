0a50cee107eb9e2eab95d4085e9796122edc6345
rancesol <rancesol@buffalo.edu>
2019-04-24 12:24:41 -0400

    Plots the spectrogram of the spiral infall with noise added into the background.

---
+++ spectrogram.py
@@ -0,0 +1,24 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+from read_plot import *
+
+np.random.seed(19680801)
+
+t, w = read_plot("omega.data")
+
+dt = 0.01
+s1 = []
+for i in range(len(t)) :
+    s1.append(np.sin(w[i]*t[i]))
+
+nse = 0.01*np.random.random(size=len(t))
+
+x = s1 + nse
+NFFT = 1024
+Fs = int(5.0 / dt)
+
+fig, (ax1, ax2) = plt.subplots(nrows=2)
+ax1.plot(t,x)
+Pxx, freqs, bins, im = ax2.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=1000)
+plt.show()
