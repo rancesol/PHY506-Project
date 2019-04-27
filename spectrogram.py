4911298c01423202b56dde807e2ba5a849172149
rancesol <rancesol@buffalo.edu>
2019-04-27 17:17:58 -0400

    Included v/c plot.

--- spectrogram.py
+++ spectrogram.py
@@ -3,22 +3,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 from read_plot import *
 
-np.random.seed(19680801)
+t, w, amp, v = read_plot("omega.data")
 
-t, w, amp = read_plot("omega.data")
-
-dt = 0.01
 s1 = []
 for i in range(len(t)) :
     s1.append(amp[i]*np.sin(w[i]*t[i]))
+    v[i] /= 3.e8
 
-nse = 0.01*np.random.random(size=len(t))
-
-x = s1 + nse
-NFFT = 1024
-Fs = int(5.0 / dt)
+x = s1
+Fs = 4096
+NFFT = int(Fs/16.0)
+NOVL = int(NFFT*15/16.0)
 
 fig, (ax1, ax2) = plt.subplots(nrows=2)
 ax1.plot(t,x, linewidth=.7)
-Pxx, freqs, bins, im = ax2.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=1000)
+ax2.plot(t,v, linewidth=.7)
+#Pxx, freqs, bins, im = ax2.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=NOVL)
+ax1.set_ylabel('Strain')
+ax1.set_xlabel('Time (s)')
+ax2.set_ylabel('v/c')
+ax2.set_xlabel('Time (s)')
+#fig.colorbar(im)
+plt.show()
+
+
+Pxx, freqs, bins, im = plt.specgram(x,NFFT=NFFT, Fs=Fs, noverlap=NOVL, xextent=[0,5])
+plt.ylabel('Frequency')
+plt.xlabel('Samples (~2*t)')
+plt.colorbar(im)
 plt.show()
